01e96fb26b32f03249ef91167ec5f1c26c073a3d
Luigi Cruz <luigifcruz@gmail.com>
2022-01-03 21:00:06 -0300

    initia julia bindings

---
+++ julia/bindings.cc
@@ -0,0 +1,15 @@
+#pragma GCC diagnostic ignored "-Wterminate"
+
+#include <fmt/core.h>
+#include <jlcxx/jlcxx.hpp>
+
+#define BL_FATAL(...) throw pybind11::value_error( \
+fmt::format("[{}@{}] {}", __FILE__, __LINE__, __VA_ARGS__).c_str());
+
+std::string greet() {
+   return "hello, world";
+}
+
+JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
+  mod.method("greet", &greet);
+}
