e5d74d6337795dfd0eb92754f461077c0e9b5254
Luigi Cruz <luigifcruz@gmail.com>
2022-01-03 21:47:48 -0300

    find julia headers

--- julia/bindings.cc
+++ julia/bindings.cc
@@ -3,13 +3,13 @@
 #include <fmt/core.h>
 #include <jlcxx/jlcxx.hpp>
 
-#define BL_FATAL(...) throw pybind11::value_error( \
-fmt::format("[{}@{}] {}", __FILE__, __LINE__, __VA_ARGS__).c_str());
+#define BL_FATAL(...) throw \
+fmt::format("[{}@{}] {}", __FILE__, __LINE__, __VA_ARGS__).c_str();
 
 std::string greet() {
    return "hello, world";
 }
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
-  mod.method("greet", &greet);
+    mod.method("greet", &greet);
 }
